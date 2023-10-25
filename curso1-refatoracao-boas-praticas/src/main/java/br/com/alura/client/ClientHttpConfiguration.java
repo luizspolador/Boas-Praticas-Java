96433e510af0d42fa20105826c13642f5bdc1ec5
Luiz Henrique Fernandes Spolador <120989515+luizspolador@users.noreply.github.com>
2023-10-25 12:21:45 -0300

    criando domains e usando a biblioteca jackson-databind

--- curso1-refatoracao-boas-praticas/src/main/java/br/com/alura/client/ClientHttpConfiguration.java
+++ curso1-refatoracao-boas-praticas/src/main/java/br/com/alura/client/ClientHttpConfiguration.java
@@ -1,5 +1,6 @@
 package br.com.alura.client;
 
+import com.google.gson.Gson;
 import com.google.gson.JsonObject;
 
 import java.io.IOException;
@@ -12,6 +13,7 @@ public class ClientHttpConfiguration {
 
     public HttpResponse<String> dispararRequisicaoGet(String uri) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .method("GET", HttpRequest.BodyPublishers.noBody())
@@ -19,13 +21,15 @@ public class ClientHttpConfiguration {
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 
-    public HttpResponse<String> dispararRequisiçãoPost(String uri, JsonObject json) throws IOException, InterruptedException {
+    public HttpResponse<String> dispararRequisicaoPost(String uri, Object object) throws IOException, InterruptedException {
         HttpClient client = HttpClient.newHttpClient();
+
         HttpRequest request = HttpRequest.newBuilder()
                 .uri(URI.create(uri))
                 .header("Content-Type", "application/json")
-                .method("POST", HttpRequest.BodyPublishers.ofString(json.toString()))
+                .method("POST", HttpRequest.BodyPublishers.ofString(new Gson().toJson(object)))
                 .build();
+
         return client.send(request, HttpResponse.BodyHandlers.ofString());
     }
 }
